780d8e7a955fc1ee59c49a54a4ec309e00a8c612
Arjun Suthar <arjunsuthar3427@gmail.com>
2024-11-28 17:37:14 +0530

    upadates in code

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import Link from "next/link";
-import {Button} from "./ui/moving-border";
+import { Button } from "./ui/moving-border";
 const HeroSection = () => {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
@@ -15,13 +15,13 @@ const HeroSection = () => {
         </h1>
         <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
           Dive into our comprehensive music courses and transform your musical
-          journey today. Whether you're a beginner or looking to refine your
+          journey today. Whether you are a beginner or looking to refine your
           skills, join us to unlock your true potential.
         </p>
         <div className="mt-4">
           <Link href={"/courses"}>
             <Button
-               borderRadius="1.75rem"
+              borderRadius="1.75rem"
               className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
             >
               Explore courses
